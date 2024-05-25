1300bbc9de6593a634e91702dbf7606f62df4e04
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-05-25 18:42:05 +0530

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import Image from "next/image";
-import { ConnectButton } from "thirdweb/react";
+import { ConnectButton, MediaRenderer, useReadContract } from "thirdweb/react";
 import thirdwebIcon from "@public/thirdweb.svg";
 import { client } from "./client";
 import { createWallet } from "thirdweb/wallets";
-import { defineChain } from "thirdweb";
+import { defineChain, getContract } from "thirdweb";
 import { baseSepolia } from "thirdweb/chains";
+import { getNFT } from "thirdweb/extensions/erc1155";
 
 export default function Home() {
 
@@ -16,6 +17,21 @@ export default function Home() {
       },
     })
   ]
+
+  const contract = getContract({
+    client: client,
+    chain: defineChain(baseSepolia),
+    address: "0xAAfeD618E4336216488131103ccbBa8d100647aC"
+  });
+
+  const {data: nft, isLoading: nftIsLoading} = useReadContract(
+    getNFT, {
+      contract: contract,
+      tokenId: 0n
+
+    }
+  );
+
   return (
     <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
       <div className="py-20">
@@ -28,6 +44,20 @@ export default function Home() {
         }}
         
         />
+
+        <div className="flex flex-col my-8">
+          {nftIsLoading ? (
+            <p> Loading... </p>
+          ) : (
+            <>
+            {nft && (
+              <MediaRenderer
+              client={client}
+              src={nft.metadata.image}
+            )}
+            </>
+          )}
+        </div>
       </div>
     </main>
   );
