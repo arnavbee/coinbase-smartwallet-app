a67d3e245a23e639a74937b5aa08392cb9df7527
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-05-26 23:03:52 +0530

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { ConnectButton, MediaRenderer, useReadContract } from "thirdweb/react";
+import { ConnectButton, MediaRenderer, TransactionButton, useActiveAccount, useReadContract } from "thirdweb/react";
 import thirdwebIcon from "@public/thirdweb.svg";
 import { client } from "./client";
 import { createWallet } from "thirdweb/wallets";
@@ -9,6 +9,8 @@ import { baseSepolia } from "thirdweb/chains";
 import { getNFT } from "thirdweb/extensions/erc1155";
 
 export default function Home() {
+
+  const account = useActiveAccount();
 
   const wallets = [
     createWallet("com.coinbase.wallet", {
@@ -34,7 +36,7 @@ export default function Home() {
 
   return (
     <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
-      <div className="py-20">
+      <div className="flex flex-col items-center py-20">
         <ConnectButton 
         client={client}
         wallets={wallets}
@@ -55,6 +57,15 @@ export default function Home() {
               client={client}
               src={nft.metadata.image}
               />
+            )}
+
+            {account ? (
+              <>
+              <p></p>
+              </>
+            ) : (
+            <p>Connect a wallet to claim your NFT</p>
+
             )}
             </>
           )}
